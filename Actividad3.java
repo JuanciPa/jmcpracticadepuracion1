dc8bf93eed81f71bf74b2a1fe2a0da7c8884a256
Jan Mendez Ceballos <janmc2004@gmail.com>
2023-11-27 15:24:25 +0000

    Práctica 1 Ejercicio y sin arreglar

---
+++ Actividad3.java
@@ -0,0 +1,18 @@
+public class Actividad3 {
+    public static void mostrartablasparcialmente(int a, int b){
+        int valor;
+        for (int i=1; i<=a; i++){
+        for (int j=1; j<=a;j++){
+            valor = i * j;
+            valor = valor+1;
+            System.out.println(i + " * " + j + " = "+ (valor+b));
+        }
+            System.out.println("--------");
+        }
+    }
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 2;
+        mostrartablasparcialmente(x, y);
+    }
+}
